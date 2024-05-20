90fdb031406adc020382e7e41a704426a37cf895
TarasovaKarina <146976177+TarasovaKarina@users.noreply.github.com>
2024-05-20 16:13:56 +0500

    Ввод данных (пока не доделана)

---
+++ Angry/begin.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        StreamReader s = new StreamReader("date.txt");
+        Throw t = new Throw(Double.Parse(s.ReadLine()), Double.Parse(s.ReadLine()), Double.Parse(s.ReadLine()), Double.Parse(s.ReadLine()), Double.Parse(s.ReadLine()), Double.Parse(s.ReadLine()), Double.Parse(s.ReadLine()));
+
+        // Подписываемся на событие
+        t.ThrowCollision += HandleCollision;
+
+        t.printThrow();
+        s.Close();
+    }
+
+
+    // Обработчик события столкновения
+    static void HandleCollision(object sender, CollisionEventArgs e)
+    {
+        Console.WriteLine($"Столкновение произошло на координатах: ({e.X}, {e.Y})");
+    }
+}
+
+// Аргументы события столкновения
+class CollisionEventArgs : EventArgs
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public CollisionEventArgs(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class Movement
+{
+    public event EventHandler<CollisionEventArgs> Collision;
+    private double V0;
+    private double Alpha0;
+    private static double g = 9.81;
+    private double h0;
+    private double x_col;
+    private double obstacleX;
+    private double obstacleY;
+    private double windSpeed;
+    private double windDirection;
+
+    public Movement(double V, double Alpha, double h, double windSpeed, double windDirection, double obstacleX, double obstacleY)
+    {
+        V0 = V;
+        Alpha0 = Alpha;
+        h0 = h;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+        this.obstacleX = obstacleX;
+        this.obstacleY = obstacleY;
+    }
+
+    public Movement()
+    {
+        V0 = 0;
+        Alpha0 = 0;
+        h0 = 0;
+        windSpeed = 0;
+        windDirection = 0;
+    }
+
+    protected virtual void OnCollision(double x, double y)
+    {
+        Collision?.Invoke(this, new CollisionEventArgs(x, y));
+    }
+
+
+    public void print()
+    {
+        Console.WriteLine(imitation(1));
+    }
+
+    Tuple<double, double> imitation(double time)
+    {
+        double x = V0 * Math.Cos(Alpha0) * time;
+        double y = h0 + V0 * Math.Sin(Alpha0) - g * time * time / 2;
+        x_col = V0 * Math.Cos(Alpha0) * (Math.Sqrt(2 * (V0 * Math.Sin(Alpha0) + h0) / g));
+        double xPrev = 0;
+        double yPrev = 0;
+        double xVel = V0 * Math.Cos(Alpha0);
+        double yVel = V0 * Math.Sin(Alpha0);
+        bool obstacleHit = false;
+
+        while (y >= 0 && !obstacleHit)
+        {
+            xPrev = x;
+            yPrev = y;
+            x += xVel * time;
+            y += yVel * time - 0.5 * g * time * time;
+
+            if (x >= obstacleX && y <= obstacleY)
+            {
+                obstacleHit = true;
+                x = obstacleX;
+                y = obstacleY;
+                OnCollision(x, y);
+            }
+
+            double windX = windSpeed * Math.Cos(windDirection);
+            double windY = windSpeed * Math.Sin(windDirection);
+            xVel = xVel + time * (windX - xVel / (Math.Sqrt(xVel * xVel + yVel * yVel)));
+            yVel = yVel + time * (-g + windY - yVel / (Math.Sqrt(xVel * xVel + yVel * yVel)));
+        }
+
+        double tCol = time * (Math.Sqrt((xPrev - x_col) * (xPrev - x_col) + (yPrev - 0) * (yPrev - 0))) / (Math.Sqrt((x - xPrev) * (x - xPrev) + (y - yPrev) * (y - yPrev)));
+        double xCol = xPrev + (x - xPrev) * tCol;
+        double yCol = yPrev + (y - yPrev) * tCol;
+        var collision = new Tuple<double, double>(xCol, yCol);
+        return collision;
+    }
+
+}
+
+class Throw
+{
+    private Movement movement;
+    public event EventHandler<CollisionEventArgs> ThrowCollision; // Событие броска
+
+    public Throw(double V, double Alpha, double h, double windSpeed, double windDirection, double obstacleX, double obstacleY)
+    {
+        movement = new Movement(V, Alpha, h, windSpeed, windDirection, obstacleX, obstacleY);
+
+        // Подписываемся на событие столкновения
+        movement.Collision += (sender, e) => ThrowCollision?.Invoke(this, e);
+
+    }
+
+
+    // Обработчик события столкновения
+    void HandleCollision(object sender, CollisionEventArgs e)
+    {
+        ThrowCollision?.Invoke(this, e);
+    }
+
+    public void printThrow()
+    {
+        movement.print();
+    }
+
+}
---
+++ Angry/input.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls; //тут находятся классы для  классических элементов управления
+using System.Windows.Input;
+using System.Windows.Media;
+namespace Petzold.ClickTheButton
+{
+    public class ClickTheButton : Window
+    {
+        TextBox[] txtbox = new TextBox[5];
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ClickTheButton());
+        }
+        public ClickTheButton()
+        {
+            Title = "Angry Birds"; //название окна
+            Button btn = new Button(); //этим классом представлена кнопка со свойством Content и событием Click
+            btn.Content = "Начать игру!";  //свойству Content объекта Button задается текстовая строка
+            btn.Click += ButtonOnClick;
+            Content = btn; //сам объект Button задаётся свойству Content объекта Window
+        }
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            EnterTheGrid();
+        }
+        void EnterTheGrid()
+        {
+            Title = "Angry Birds";
+            MinWidth = 350;
+            SizeToContent = SizeToContent.WidthAndHeight;
+            // Создание объекта StackPanel для содержимого окна
+            StackPanel stack = new StackPanel();
+            Content = stack;
+            // Создание объекта Grid и его добавление в StackPanel.
+            Grid grid1 = new Grid();
+            grid1.Margin = new Thickness(5);
+            stack.Children.Add(grid1);
+            // Создание определений строк
+            for (int i = 0; i < 5; i++)
+            {
+                RowDefinition rowdef = new RowDefinition();
+                rowdef.Height = GridLength.Auto;
+                grid1.RowDefinitions.Add(rowdef);
+            }
+            // Создание определений столбцов
+            ColumnDefinition coldef = new ColumnDefinition();
+            coldef.Width = GridLength.Auto;
+            grid1.ColumnDefinitions.Add(coldef);
+            coldef = new ColumnDefinition();
+            coldef.Width = new GridLength(100, GridUnitType.Star);
+            grid1.ColumnDefinitions.Add(coldef);
+            // Создание надписей и текстовых полей
+            string[] strLabels = { "Скорость:",  "Угол наклона:",
+                "Высота:",
+                "Обцисса препядствия:",
+                "Ордината препядствия:" };
+            for (int i = 0; i < strLabels.Length; i++)
+            {
+                Label lbl = new Label();
+                lbl.Content = strLabels[i];
+                //Выравнивание текстовых полей по центру
+                lbl.VerticalContentAlignment = VerticalAlignment.Center;
+                grid1.Children.Add(lbl);
+                //Количество столбцов и строк, занимаемых элментом, задаётся статистическими методами SetRow и SetColumn  
+                Grid.SetRow(lbl, i);
+                Grid.SetColumn(lbl, 0);
+                txtbox[i] = new TextBox();
+                txtbox[i].Margin = new Thickness(5);
+                grid1.Children.Add(txtbox[i]);
+                Grid.SetRow(txtbox[i], i);
+                Grid.SetColumn(txtbox[i], 1);
+            }
+            // Создание второго объекта Grid и добавление в  StackPanel.
+            Grid grid2 = new Grid();
+            grid2.Margin = new Thickness(10);
+            stack.Children.Add(grid2);
+            // Для одной строки создавать определение не обязательно     
+            // В определениях строк по умолчанию используется режим "star"
+            grid2.ColumnDefinitions.Add(new ColumnDefinition());
+            grid2.ColumnDefinitions.Add(new ColumnDefinition());
+            // Создание кнопок
+            Button btn = new Button();
+            btn.Content = "Submit";
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.IsDefault = true;
+            btn.Click += delegate { new Movement(txtbox[0].Text) };//дописать функцию!
+            grid2.Children.Add(btn);  // Row &  column are 0.
+            btn = new Button();
+
+            //MessageBox.Show(txtbox[0].Text);
+            /*btn.Content = "Cancel";
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.IsCancel = true;
+            btn.Click += delegate { Close(); };
+            grid2.Children.Add(btn);
+            Grid.SetColumn(btn, 1);  // Row is 0.          
+                                     // Передача фокуса первому текстовому полю.*/
+            (stack.Children[0] as Panel).Children[1].Focus();
+        }
+    }
+}
